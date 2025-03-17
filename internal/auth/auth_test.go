5532753e6d807c7a1feb525061fb7a675849e778
crabmustard <bryce.lippert@gmail.com>
2025-03-17 17:03:13 -0400

    breaking tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,48 @@
+package auth
+
+import (
+	"bytes"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", bytes.NewBufferString(""))
+
+	testValue := "11122224333"
+	req.Header.Add("Authorization", "ApiKey "+testValue)
+
+	recorder, _ := GetAPIKey(req.Header)
+
+	if recorder != testValue {
+		t.Errorf("Expected %s, got %s", testValue, recorder)
+	}
+
+}
+
+func TestGetAPIKeyNoAuth(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", bytes.NewBufferString(""))
+
+	_, err := GetAPIKey(req.Header)
+
+	if !errors.Is(err, ErrNoAuthHeaderIncluded) {
+		t.Errorf("Expected %s, got %s", ErrNoAuthHeaderIncluded, err)
+	}
+
+}
+
+func TestGetAPIKeyBadAuth(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", bytes.NewBufferString(""))
+
+	testValue := "11122224333"
+	req.Header.Add("Authorization", "ApiKey "+testValue)
+
+	_, err := GetAPIKey(req.Header)
+
+	if err.Error() != "malformed authorization header" {
+		t.Errorf("Expected malformed authorization header, got %s", err.Error())
+	}
+
+}
